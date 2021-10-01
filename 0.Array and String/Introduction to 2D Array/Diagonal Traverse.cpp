d85a20190a9a0c158f0fc755bc36d410334e0c69
phattran <tranquangphatsitrum@gmail.com>
2021-10-01 17:29:48 +0700

    Review 2D array

--- 0.Array and String/Introduction to 2D Array/Diagonal Traverse.cpp
+++ 0.Array and String/Introduction to 2D Array/Diagonal Traverse.cpp
@@ -1,22 +1,55 @@
 class Solution {
 public:
-    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        int dong = mat.size();
-        int cot = mat[0].size();
-        vector<int> result(0);
-        vector<vector<int>> cheo(dong + cot + 1, vector<int>(0, 0));
-        for (int i = 0; i < dong; i++) {
-            for (int j = 0; j < cot; j++) {
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat){
+        vector<int> result;
+        bool isUp= 1;
+        int numberOfColumns=mat[0].size();
+        int numberOfRows=mat.size();
+        if (numberOfRows==1) return mat[0];
+        int i=0,j=0;
+        
+        while(result.size() != numberOfColumns*numberOfRows){
+            result.push_back(mat[i][j]);
+            if (!isUp){
+                //if end of diagonal DOWN
+                if (j==0 || i==numberOfRows-1) {
+                    isUp=1;
+                //if touch the bottom row
+                    if (i != numberOfRows-1){
+                        i++;
+                    }
+                //if touch the first column
+                    else{
+                        j++;
+                    }
+                }
+                else{
+                    i++;
+                    j--;
+                }
 
-                cheo[i + j].push_back(mat[i][j]);
+            }
+            else if (isUp){
+                //if end of diagonal UP
+                if (i==0  || j== numberOfColumns-1) {
+                    isUp=0;
+                //if touch the top row
+                    if(j != numberOfColumns-1){
+                        j++;
+                    }
+                //if touch the last column
+                    else{
+                        i++;
+                    }
+                    isUp=0;
+                }
+                else{
+                    i--;
+                    j++;
+                }
             }
         }
-        for (int k = 0; k < dong + cot; k++) {
-            if (k % 2 == 0) {
-                reverse(cheo[k].begin(), cheo[k].end());
-            }
-            copy(cheo[k].begin(), cheo[k].end(), back_inserter(result));
-        }
+
         return result;
     }
 };
