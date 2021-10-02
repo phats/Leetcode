d8810c48de05b11b07d6d6d84e57ea3336531a41
truongcntn2017 <1712195@student.hcmus.edu.vn>
2021-10-02 10:41:25 +0700

    mod largest_number.cpp

--- 0.Array and String/Introduce to Array/Largest Number At Least Twice of Others.cpp
+++ 0.Array and String/Introduce to Array/Largest Number At Least Twice of Others.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     int dominantIndex(vector<int>& nums) {
-        if (nums.size()==1) return 0;
+        if (nums.size()==1) 
+            return 0;
+        else {
 
-        int firstMaximumNumber=INT_MIN;
-        int indexMaxNumber=0;
-        int secondMaximumNumber=INT_MIN;
+            int firstMaximumNumber=INT_MIN;
+            int indexMaxNumber=0;
+            int secondMaximumNumber=INT_MIN;
 
-        for (int i=0;i<nums.size();i++){
-            if (nums[i]>secondMaximumNumber){
-                if (nums[i]>firstMaximumNumber){
-                    secondMaximumNumber=firstMaximumNumber;
-                    firstMaximumNumber=nums[i];
-                    indexMaxNumber=i;
-                }
-                else{
-                    secondMaximumNumber=nums[i];
+            for (int i=0;i<nums.size();i++){
+                if (nums[i]>secondMaximumNumber){
+                    if (nums[i]>firstMaximumNumber){
+                        secondMaximumNumber=firstMaximumNumber;
+                        firstMaximumNumber=nums[i];
+                        indexMaxNumber=i;
+                    }
+                    else
+                        secondMaximumNumber=nums[i];
                 }
             }
-        }
 
-        if (firstMaximumNumber>=secondMaximumNumber*2) return indexMaxNumber;
-        
-        return -1;
+            if (firstMaximumNumber>=secondMaximumNumber*2)
+                return indexMaxNumber;
+            else 
+                return -1;
+        }
     }
 };
